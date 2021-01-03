7434238aeaa1b4b7325c45f3b74d49569d5f47b0
Sammy Robens-Paradise <srobensparadise@gmail.com>
2021-01-02 23:13:06 -0500

    enabled uploading to s3

--- app/utils/images/image-handlers.js
+++ app/utils/images/image-handlers.js
@@ -1,31 +1,37 @@
-import { SECURE_KEY } from '@env'
-import axios from 'axios'
-import { API_BASE_URL } from '../urls/urls'
-import ImgToBase64 from 'react-native-image-base64'
-
-export const getBlob = async (dataUri) => {
-  const encodedImage = `data:image/jpeg;base64,${await ImgToBase64.getBase64String(dataUri)}`
-  return encodedImage
-}
-
-export const getImageInfo = (file) => {
-  const { fileName, type, uri } = file
-  const File = { name: fileName, uri: uri, type: type }
-  const body = new FormData()
-  body.append('file', File)
-  return body
-}
+import { S3 } from 'aws-sdk'
+import { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_IMAGE_BUCKET_NAME } from '@env'
+import { decode } from 'base64-arraybuffer'
+var fs = require('react-native-fs')
 
 export const uploadImageToS3 = async (file) => {
-  const imageBody = getImageInfo(file)
-  const uploadedToS3 = await axios.put(
-    `${API_BASE_URL}/upload`,
-    { file: imageBody },
-    {
-      headers: {
-        Authorization: `Bearer ${SECURE_KEY}`,
-      },
-    },
-  )
-  console.log(uploadedToS3)
+  let responseData
+  const s3Bucket = new S3({
+    accessKeyId: `${AWS_ACCESS_KEY_ID}`,
+    secretAccessKey: `${AWS_SECRET_ACCESS_KEY}`,
+    Bucket: `${AWS_IMAGE_BUCKET_NAME}`,
+    signatureVersion: 'v4',
+  })
+  let contentType = 'image/jpeg'
+  let contentDeposition = 'inline;filename="' + file.fileName + '"'
+  const base64 = await fs.readFile(file.uri, 'base64')
+  const arrayBuffer = decode(base64)
+  s3Bucket.createBucket(() => {
+    const params = {
+      Bucket: `${AWS_IMAGE_BUCKET_NAME}`,
+      Key: file.fileName,
+      Body: arrayBuffer,
+      ContentDisposition: contentDeposition,
+      ContentType: contentType,
+    }
+    s3Bucket.upload(params, (err, data) => {
+      if (err) {
+        console.error('Unable to upload Image')
+        alert('Unable to Upload Image')
+      } else {
+        console.log(data)
+        responseData = data
+      }
+    })
+  })
+  return responseData
 }
