467e87ae70db8a18804256b3ad2f43364b8e2d9c
Sammy Robens-Paradise <srobensparadise@gmail.com>
2021-01-18 14:32:56 -0500

    rollback point

--- app/src/core/Viewer.tsx
+++ app/src/core/Viewer.tsx
@@ -11,34 +11,45 @@ interface LoadingProps {
 }
 
 const ViewerPage = ({ route }: LoadingProps): JSX.Element => {
-    const { search } = route.params;
-    const { webPages } = search.data;
-    if (webPages !== undefined) {
-        const { value: pages, totalEstimatedMatches } = webPages;
-        const typePages: Array<SearchPreviewReference> = pages;
-        return (
-            <SafeAreaView style={styles.container}>
-                <ScrollView>
+    if (route?.params) {
+        const { search } = route.params;
+        const { webPages } = search?.data;
+        if (webPages !== undefined) {
+            const { value: pages, totalEstimatedMatches } = webPages;
+            const typePages: Array<SearchPreviewReference> = pages;
+            return (
+                <SafeAreaView style={styles.container}>
+                    <ScrollView>
+                        <View style={{ alignItems: 'center', marginTop: 40 }}>
+                            <Text>{`Showing top ${typePages?.length} of ${totalEstimatedMatches} Results`}</Text>
+                        </View>
+                        {typePages.length > 0 ? (
+                            typePages.map((s) => {
+                                return <WebpagePreview search={s} key={s.id} />;
+                            })
+                        ) : (
+                            <Text>No Search Results Detected</Text>
+                        )}
+                    </ScrollView>
+                </SafeAreaView>
+            );
+        } else {
+            return (
+                <SafeAreaView style={styles.container}>
                     <View style={{ alignItems: 'center', marginTop: 40 }}>
-                        <Text>{`Showing top ${typePages?.length} of ${totalEstimatedMatches} Results`}</Text>
+                        <Text>
+                            No Search Results Detected. Try cropping the
+                            picture.
+                        </Text>
                     </View>
-                    {typePages.length > 0 ? (
-                        typePages.map((s) => {
-                            return <WebpagePreview search={s} key={s.id} />;
-                        })
-                    ) : (
-                        <Text>No Search Results Detected</Text>
-                    )}
-                </ScrollView>
-            </SafeAreaView>
-        );
+                </SafeAreaView>
+            );
+        }
     } else {
         return (
             <SafeAreaView style={styles.container}>
                 <View style={{ alignItems: 'center', marginTop: 40 }}>
-                    <Text>
-                        No Search Results Detected. Try cropping the picture.
-                    </Text>
+                    <Text>Choose an Image to begin a Search</Text>
                 </View>
             </SafeAreaView>
         );
