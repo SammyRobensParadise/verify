98e6a3a8059a6e1733d110dd17408de29e6e71ab
Sammy Robens-Paradise <srobensparadise@gmail.com>
2021-01-18 21:50:09 -0500

    added styling

--- app/src/core/CoreApp.tsx
+++ app/src/core/CoreApp.tsx
@@ -3,11 +3,13 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 import HomePage from './Home';
 import SettingsPage from './Settings';
 import LoadingPage from './Loading';
 import ViewerPage from './Viewer';
+import theme from '../../components/theme/theme';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -15,14 +17,41 @@ const Stack = createStackNavigator();
 const Core = ({ route }: { route: any }) => {
     const { authUser } = route.params;
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Verify ⚡️" component={HomePage} />
+        <Tab.Navigator
+            tabBarOptions={{
+                labelStyle: {
+                    color: theme.colors.black
+                },
+                style: {
+                    paddingTop: 10
+                }
+            }}
+        >
             <Tab.Screen
-                name="Settings ⚙️"
+                name="Verify"
+                component={HomePage}
+                options={{
+                    tabBarLabel: 'Verify',
+                    tabBarIcon: () => <AntDesign name="camerao" size={20} />
+                }}
+            />
+            <Tab.Screen
+                name="Settings"
                 component={SettingsPage}
                 initialParams={{ authUser: authUser }}
+                options={{
+                    tabBarLabel: 'Settings',
+                    tabBarIcon: () => <AntDesign name="setting" size={20} />
+                }}
             />
-            <Stack.Screen name="Viewer" component={ViewerPage} />
+            <Tab.Screen
+                name="Viewer"
+                component={ViewerPage}
+                options={{
+                    tabBarLabel: 'View',
+                    tabBarIcon: () => <AntDesign name="infocirlceo" size={20} />
+                }}
+            />
         </Tab.Navigator>
     );
 };
