afe1de00c71ab50f9f7deaf26657bc2ad941ce87
Sammy Robens-Paradise <srobensparadise@gmail.com>
2021-01-25 21:42:32 -0500

    created auth context

--- app/utils/auth/auth-context.tsx
+++ app/utils/auth/auth-context.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import Auth0 from 'react-native-auth0';
-import { Auth0Credentials, UserInfoType } from 'types/types';
-var credentials: Auth0Credentials = require('../../auth0-configuration');
-const auth0 = new Auth0(credentials);
+import { UserInfoType } from 'types/types';
+import {
+    _getUserInfo,
+    _onLogin,
+    _onLoginWithInfo,
+    _onLogout
+} from './auth-handlers';
 
 const AuthContext = React.createContext(null);
+
 const AuthActionId = {
     LOGIN: 1,
     LOGIN_SUCCESS: 2,
-    LOGIN_WITH_INFO: 3,
-    LOGIN_WITHINFO_SUCCESS: 4,
-    LOGOUT: 5,
-    LOGOUT_SUCCESS: 6,
-    GET_USER_INFO: 7,
-    GET_USER_INFO_SUCCESS: 8,
-    GET_BASE_AUTH_URL: 9
+    LOGIN_ERROR: 3,
+    LOGIN_WITH_INFO: 4,
+    LOGIN_WITHINFO_SUCCESS: 5,
+    LOGIN_WITHINFO_ERROR: 6,
+    LOGOUT: 7,
+    LOGOUT_SUCCESS: 8,
+    LOGOUT_FAILURE: 9,
+    GET_USER_INFO: 10,
+    GET_USER_INFO_SUCCESS: 11,
+    GET_USER_INFO_ERROR: 12,
+    GET_BASE_AUTH_URL: 13
 };
 
 type AuthState = {
@@ -43,28 +51,114 @@ const AuthReducer = (state: AuthState, action: ActionType) => {
                 isLoggedIn: false
             };
         }
+        case AuthActionId.LOGIN_SUCCESS: {
+            return {
+                ...state,
+                isLoading: false,
+                isLoggedIn: true
+            };
+        }
+        case AuthActionId.LOGIN_ERROR: {
+            return {
+                ...state,
+                isLoading: false,
+                isLoggedIn: false,
+                error: true
+            };
+        }
+        case AuthActionId.LOGIN_WITH_INFO: {
+            return {
+                ...state,
+                isLoading: true,
+                isLoggedIn: false
+            };
+        }
+        case AuthActionId.LOGIN_WITHINFO_SUCCESS: {
+            return {
+                ...state,
+                isLoading: false,
+                isLoggedIn: true
+            };
+        }
+        case AuthActionId.LOGIN_WITHINFO_ERROR: {
+            return {
+                ...state,
+                isLoading: false,
+                isLoggedIn: false,
+                error: true
+            };
+        }
+        case AuthActionId.LOGOUT: {
+            return {
+                ...state,
+                isLoading: true,
+                isLoggedIn: true
+            };
+        }
+        case AuthActionId.LOGOUT_SUCCESS: {
+            return {
+                ...state,
+                isLoading: false,
+                isLoggedIn: false
+            };
+        }
+        case AuthActionId.LOGOUT_FAILURE: {
+            return {
+                ...state,
+                isLoading: false,
+                isLoggedIn: true
+            };
+        }
+        case AuthActionId.GET_USER_INFO: {
+            return {
+                ...state,
+                isLoading: true,
+                isLoggedIn: true
+            };
+        }
+        case AuthActionId.GET_USER_INFO_SUCCESS: {
+            return {
+                ...state,
+                isLoading: false,
+                isLoggedIn: true
+            };
+        }
+        case AuthActionId.GET_USER_INFO_ERROR: {
+            return {
+                ...state,
+                isLoading: false,
+                isLoggedIn: true
+            };
+        }
         default: {
             throw new Error(`Unsupported action type: ${action.type}`);
         }
     }
 };
 
-const loginUser = (dispatch: React.Dispatch<ActionType>) => async () => {
+const login = (dispatch: React.Dispatch<ActionType>) => async () => {
     dispatch({ type: AuthActionId.LOGIN, payload: {} });
-    const raw = await auth0.webAuth
-        .authorize({
-            scope: 'openid profile email'
-        })
-        .then((credentials: any) => {
-            return credentials;
-        })
-        .catch((error: Error) => {
-            return error;
-        });
-    if (!raw.email) {
-        return;
-    }
+    const raw = await _onLogin();
     dispatch({ type: AuthActionId.LOGIN_SUCCESS, payload: raw });
+};
+
+const loginInfo = (dispatch: React.Dispatch<ActionType>) => async () => {
+    dispatch({ type: AuthActionId.LOGIN_WITH_INFO, payload: {} });
+    const raw = await _onLoginWithInfo();
+    dispatch({ type: AuthActionId.LOGIN_WITHINFO_SUCCESS, payload: raw });
+};
+
+const logout = (dispatch: React.Dispatch<ActionType>) => async () => {
+    dispatch({ type: AuthActionId.LOGOUT, payload: {} });
+    const raw = await _onLogout();
+    dispatch({ type: AuthActionId.LOGOUT_SUCCESS, payload: raw });
+};
+
+const user = (dispatch: React.Dispatch<ActionType>) => async () => {
+    dispatch({ type: AuthActionId.GET_USER_INFO, payload: {} });
+    const res = await _onLoginWithInfo();
+    const raw = await _getUserInfo(res.accessToken);
+    dispatch({ type: AuthActionId.GET_USER_INFO_SUCCESS, payload: raw });
 };
 
 type AuthProviderProps = {
@@ -88,6 +182,9 @@ export const useAuth = () => {
     return {
         state,
         dispatch,
-        loginUser: loginUser(dispatch)
+        login: login(dispatch),
+        loginInfo: loginInfo(dispatch),
+        logout: logout(dispatch),
+        user: user(dispatch)
     };
 };
